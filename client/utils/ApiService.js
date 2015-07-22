b186cc25c1ca2d99f72069170d6b4f71c5cf8ca1
Kang Peng <kangpeng@MA-DIGXP.local>
2015-07-22 12:05:25 -0400

    Moved Hero data to API.

--- client/utils/ApiService.js
+++ client/utils/ApiService.js
@@ -17,6 +17,22 @@ const API = {
   getFilters() {
     const filterList = filters.filters;
     return filterList;
+  },
+  getHero () {
+    const bookIntros = {
+      adult: {
+        bookTitle: 'The Amazing Adventures of Kavalier & Clay',
+        quote: '"I loved this book. It\'s a great story with complex, interesting characters in a fascinating setting. The creation of the comic book history is not only fascinating but the fictional elements are so well integrated that without looking it up."'
+      },
+      youngAdult: {
+        bookTitle: 'The Six-Gun Tarot',
+        quote: '"The wild--and weird--west! The strange town of Golgotha with its even stranger citizens isn\'t the sort of place to visit . . . but makes for great reading!"'
+      },
+      child: {
+        bookTitle: 'The Whispering Skull',
+        quote: '"Ghost-hunting, murder and mysteries, oh my! This second book in the author\'s Lockwood & Co. series delivers just as much suspenseful, spine-tingling action."'
+      }
+    } 
   }
 };
 
