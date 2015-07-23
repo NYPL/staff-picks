d57d14cbfbf12f6adf8aa5b5ee901d3f9108e4b0
Kang Peng <kangpeng@MA-DIGXP.local>
2015-07-23 11:55:33 -0400

    Set font size.

--- client/components/Error404/Error404.jsx
+++ client/components/Error404/Error404.jsx
@@ -13,16 +13,21 @@ class Error extends React.Component {
   render () {
   	return (
       <div class='error-404'>
-        <h3>We&#39;re sorry...</h3>
-        <p>The page you requested is either unavailable or you need permission to view the content.</p>
-        <p>If you can&#39;t find the page you&#39;re looking for, please try our <a href='http://www.nypl.org/ask-nypl'>ASK NYPL</a> service.</p>
+        <h1 style={styles.ErrorTitle}>We&#39;re sorry...</h1>
+        <p style={styles.ErrorContent}>The page you requested is either unavailable or you need permission to view the content.</p>
+        <p style={styles.ErrorContent}>If you can&#39;t find the page you&#39;re looking for, please try our <a href='http://www.nypl.org/ask-nypl'>ASK NYPL</a> service.</p>
       </div>
 		);
   }
 }
 
 const styles = {
-
+  ErrorTitle: {
+    fontSize: '2.4rem'
+  },
+  ErrorContent: {
+    fontSize: '1.4rem'
+  }
 };
 
 export default Radium(Error);
