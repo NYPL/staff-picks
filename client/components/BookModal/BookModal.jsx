1ec90c81133afb056c7dd71557acd9882c1c1715
Kang Peng <kangpeng@MA-DIGXP.local>
2015-08-07 13:16:43 -0400

    Changed bookModal mega tags.

--- client/components/BookModal/BookModal.jsx
+++ client/components/BookModal/BookModal.jsx
@@ -70,9 +70,10 @@ let BookModal = React.createClass({
 
   render: function() {
     let book = this.state.book,
-      title = '',
+      title = 'Staff Picks | The New York Public Library',
       imageSrc = '/client/images/staff_pic_bg.jpg',
-      description = '',
+      description = 'Every month NYPL\'s librarians share their favorite reads.',
+      bookId,
       imageLink = `https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?` +
        `&userID=NYPL49807&password=CC68707&Value=${imageSrc}&content=M&Return=1&Type=M`;
     
@@ -80,21 +81,22 @@ let BookModal = React.createClass({
       title = book['staff-pick-item']['attributes']['title'];
       description = book.attributes.text;
       imageSrc = book['staff-pick-item']['attributes']['image-slug'];
+      bookId= book['staff-pick-item']['id'];
     }
 
     var tags = [
-      {property: "og:title", content: 'Staff Picks | The New York Public Library'},
+      {property: "og:title", content: title},
       {property: "og:type", content: 'website'},
-      {property: "og:image", content: '/client/images/staff_pic_bg.jpg'},
-      {property: "og:description", content: 'Every month NYPL\'s librarians share their favorite reads.'},
+      {property: "og:image", content: imageLink},
+      {property: "og:description", content: description},
       {property: "og:site_name", content: 'Staff Picks | The New York Public Library'},
-      {property: "og:url", content: 'http://nypl.org/staff-picks'},
+      {property: "og:url", content: `http://nypl.org/staff-picks/${bookId}`},
       {name: "twitter:card", content: 'summary_large_image'},
       {name: "twitter:site", content: '@nypl'},
-      {name: "twitter:title", content: 'Staff Picks | The New York Public Library'},
-      {name: "twitter:description", content: 'Every month NYPL\'s librarians share their favorite reads.'},
+      {name: "twitter:title", content: title},
+      {name: "twitter:description", content: description},
       {name: "twitter:creator", content: '@nypl'},
-      {name: "twitter:image", content: '/client/images/staff_pic_bg.jpg'}
+      {name: "twitter:image", content: imageSrc}
     ];
 
     return (
