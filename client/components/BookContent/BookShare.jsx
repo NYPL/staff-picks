51b4e869da968e54337f5a8645dd2d0adadc24af
Kang Peng <kangpeng@MA-DIGXP.local>
2015-08-13 15:54:58 -0400

    Added show-via on tumblr

--- client/components/BookContent/BookShare.jsx
+++ client/components/BookContent/BookShare.jsx
@@ -14,7 +14,8 @@ class BookShare extends React.Component {
       facebook: `http://www.facebook.com/sharer.php?u=${nyplStaffPick}&t=${nyplStaffPick}`,
       twitter: `https://twitter.com/intent/tweet?text=${nyplStaffPick}&url=${shareUrl}&via=${via}`,
       tumblr: `https://www.tumblr.com/widgets/share/tool?posttype=link&canonicalUrl=${shareUrl}` +
-      `&title=NYPL%20%7C%20Staff%20Picks&caption=Every+month%2C+NYPL%27s+book+experts+share+100+titles+they+love.`
+      `&title=NYPL%20%7C%20Staff%20Picks&caption=Every+month%2C+NYPL%27s+book+experts+share+100+titles+they+love.` +
+      `&show-via=${via}`
     }
   }
 
