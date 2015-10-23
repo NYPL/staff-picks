863c586de323ffaf966009e580f601afbb0b222f
Edwin Guzman <edwin.gzmn@gmail.com>
2015-10-23 14:49:39 -0400

    Adding header 1.0.5 - myNypl.

--- client/stores/HeaderStore.js
+++ client/stores/HeaderStore.js
@@ -9,18 +9,24 @@ class HeaderStore {
       handleFetchHeaderData: Actions.FETCH_HEADER_DATA,
       handleHeaderDataFailedFetch: Actions.FAILED_HEADER_DATA,
       handleSetMobileMenuButtonValue: Actions.SET_MOBILE_MENU_BUTTON_VALUE,
+      handleSetMobileMyNyplButtonValue: Actions.SET_MOBILE_MY_NYPL_BUTTON_VALUE,
       handleSearchButtonActionValue: Actions.SEARCH_BUTTON_ACTION_VALUE,
       handleUpdateIsHeaderSticky: Actions.UPDATE_IS_HEADER_STICKY,
       handleSetLastActiveMenuItem: Actions.SET_LAST_ACTIVE_MENU_ITEM,
-      handleToggleSubscribeFormVisible: Actions.TOGGLE_SUBSCRIBE_FORM_VISIBLE
+      handleSetClientAppEnv: Actions.SET_CLIENT_APP_ENV,
+      handleToggleSubscribeFormVisible: Actions.TOGGLE_SUBSCRIBE_FORM_VISIBLE,
+      handleToggleMyNyplVisible: Actions.TOGGLE_MY_NYPL_VISIBLE
     });
 
     this.exportPublicMethods({
       _getMobileMenuBtnValue: this._getMobileMenuBtnValue,
       _getSearchButtonActionValue: this._getSearchButtonActionValue,
+      _getMobileMyNyplButtonValue: this._getMobileMyNyplButtonValue,
       _getIsStickyValue: this._getIsStickyValue,
       _getLastActiveMenuItem: this._getLastActiveMenuItem,
-      _getSubscribeFormVisible: this._getSubscribeFormVisible
+      _getSubscribeFormVisible: this._getSubscribeFormVisible,
+      _getMyNyplVisible: this._getMyNyplVisible,
+      _getClientAppEnv: this._getClientAppEnv
     });
 
     this.state = {
@@ -30,7 +36,10 @@ class HeaderStore {
       lastActiveMenuItem: '',
       activeMobileButton: '',
       searchButtonAction:'',
-      subscribeFormVisible: false
+      mobileMyNyplButton: '',
+      subscribeFormVisible: false,
+      myNyplVisible: false,
+      clientAppEnv: ''
     };
   }
 
@@ -46,13 +55,33 @@ class HeaderStore {
   }
 
   /**
+   * _getMobileMyNyplButtonValue()
+   * returns the current state.mobileMyNyplButton
+   * value.
+   * @return {String}
+   */
+  _getMobileMyNyplButtonValue() {
+    return this.state.mobileMyNyplButton;
+  }
+
+  /**
    * _getSubscribeFormVisible()
    * returns the current state.subscribeFormVisible
    * value.
-   * @return {Boolean} true/falsse
+   * @return {Boolean} true/false
    */
   _getSubscribeFormVisible() {
     return this.state.subscribeFormVisible;
+  }
+
+  /**
+   * _getMyNyplVisible()
+   * returns the current state.myNYPLVisible
+   * value.
+   * @return {Boolean} true/false
+   */
+  _getMyNyplVisible() {
+    return this.state.myNyplVisible;
   }
 
   /**
@@ -85,6 +114,10 @@ class HeaderStore {
     return this.state.lastActiveMenuItem;
   }
 
+  _getClientAppEnv() {
+    return this.state.clientAppEnv;
+  }
+
   /*** PRIVATE METHODS ***/
   handleUpdateHeaderData(data) {
     this.setState({headerData: data});
@@ -102,6 +135,10 @@ class HeaderStore {
     this.setState({activeMobileButton: currentActiveMobileButton});
   }
 
+  handleSetMobileMyNyplButtonValue(value) {
+    this.setState({mobileMyNyplButton: value});
+  }
+
   // The set search button action value to Store
   handleSearchButtonActionValue(actionValue) {
     this.setState({searchButtonAction: actionValue});
@@ -115,8 +152,16 @@ class HeaderStore {
     this.setState({lastActiveMenuItem: value});
   }
 
+  handleSetClientAppEnv(value) {
+    this.setState({clientAppEnv: value});
+  }
+
   handleToggleSubscribeFormVisible(value) {
     this.setState({subscribeFormVisible: value});
+  }
+
+  handleToggleMyNyplVisible(value) {
+    this.setState({myNyplVisible: value});
   }
 }
 
