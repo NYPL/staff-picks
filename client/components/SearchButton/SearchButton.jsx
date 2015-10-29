cac9c528849ca6d0dca75449cd33041740f4777a
Edwin Guzman <edwin.gzmn@gmail.com>
2015-10-29 16:55:05 -0400

    Updating with header v1.0.5 - CSS tweaks, border bottom, subnav title linked.

--- client/components/SearchButton/SearchButton.jsx
+++ client/components/SearchButton/SearchButton.jsx
@@ -6,8 +6,12 @@ import cx from 'classnames';
 import BasicButton from '../Buttons/BasicButton.jsx';
 import SearchBox from '../SearchBox/SearchBox.jsx';
 
+// ALT Flux Store/Actions
 import HeaderStore from '../../stores/HeaderStore.js';
 import HeaderActions from '../../actions/HeaderActions.js';
+
+// GA Utility Library
+import gaUtils from '../../utils/gaUtils.js';
 
 // Create React class
 class SearchButton extends React.Component {
@@ -46,9 +50,11 @@ class SearchButton extends React.Component {
    * with hoverSearch after a set time delay.
    */
   _activateHover() {
-
     this.hoverTimer = setTimeout(() => {
       HeaderActions.searchButtonActionValue('hoverSearch');
+
+      // Fire GA event to track when the Search Menu is open
+      gaUtils._trackEvent('Search', 'Open Menu');
     }, 350);
   }
 
