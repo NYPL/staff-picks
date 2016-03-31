64fd16dfbca2d60e26f8e5d79f273b1419227ebf
Sean Redmond <github-smr@sneakemail.com>
2016-03-31 15:59:15 -0400

    Break up let statements
    Break up the multiple let statement and change to const (WEB-2001)

--- src/app/utils/DateService.js
+++ src/app/utils/DateService.js
@@ -3,12 +3,12 @@ function staffPicksDate(date) {
     return {month, year};
   }
 
-  let d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/),
-    monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'],
-    newDate = new Date(d[1], d[2] - 1, d[3]),
-    month = monthsArr[newDate.getMonth()],
-    year = newDate.getFullYear();
+  const d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/);
+  const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
+		     'July', 'August', 'September', 'October', 'November', 'December'];
+  const newDate = new Date(d[1], d[2] - 1, d[3]);
+  const month = monthsArr[newDate.getMonth()];
+  const year = newDate.getFullYear();
 
   return {
     month,
