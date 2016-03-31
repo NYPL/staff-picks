66dbc814375e0e74daf166bfda86b0a5204d1c68
Sean Redmond <github-smr@sneakemail.com>
2016-03-31 16:10:06 -0400

    Use seasons starting in 2016
    Add a function to examine the date and return the month name up until
    2016, and for dates in 2016 and later turn the month into a season. (WEB-2001)

--- src/app/utils/DateService.js
+++ src/app/utils/DateService.js
@@ -1,15 +1,30 @@
+function monthOrSeason(month, year) {
+  const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
+		     'July', 'August', 'September', 'October', 'November',
+		     'December'];
+
+  // Unhappy hack. Starting with March 2016 the lists are seasonal ("Spring"
+  // etc.) This implementation should perhaps be replaced with a different
+  // data model that allows for arbitrary naming of lists.
+  if (year >= 2016) {
+    if (month >= 2) {
+      return "Spring";
+    }
+  }
+
+  return monthsArr[month];
+}
+
 function staffPicksDate(date) {
   if (!date) {
     return {month, year};
   }
 
   const d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/);
-  const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
-		     'July', 'August', 'September', 'October', 'November', 'December'];
   const newDate = new Date(d[1], d[2] - 1, d[3]);
-  const month = monthsArr[newDate.getMonth()];
   const year = newDate.getFullYear();
-
+  const month = monthOrSeason(newDate.getMonth(), year);
+ 
   return {
     month,
     year
