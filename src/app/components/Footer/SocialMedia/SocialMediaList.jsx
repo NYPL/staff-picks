59c97c01301ee1cb16322915eafc0cdafd02daa8
Sean Redmond <github-smr@sneakemail.com>
2016-04-04 10:13:43 -0400

    Lint'd

--- src/app/components/Footer/SocialMedia/SocialMediaList.jsx
+++ src/app/components/Footer/SocialMedia/SocialMediaList.jsx
@@ -11,18 +11,18 @@ class SocialMediaList extends React.Component {
   }
 
   render () {
-  	let SocialMediaNodes = this.props.data.map(function (node, i) {
-  		return (
-  			<SocialMediaNode name={node.name} link={node.link} className={node.className} key={i} />
-  		);
-  	});
+    let SocialMediaNodes = this.props.data.map(function (node, i) {
+      return (
+        <SocialMediaNode name={node.name} link={node.link} className={node.className} key={i} />
+      );
+    });
 
-		return (
-			<ul className={this.props.className} style={this.props.style}>
-				{SocialMediaNodes}
-			</ul>
-		);
-	}
+    return (
+      <ul className={this.props.className} style={this.props.style}>
+      {SocialMediaNodes}
+      </ul>
+    );
+  }
 }
 
 export default Radium(SocialMediaList);
