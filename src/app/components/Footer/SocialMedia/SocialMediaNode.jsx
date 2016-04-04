99392d7e0e1f6246b7500ef08ca0965c15265392
Sean Redmond <github-smr@sneakemail.com>
2016-04-04 10:15:22 -0400

    Lint'd

--- src/app/components/Footer/SocialMedia/SocialMediaNode.jsx
+++ src/app/components/Footer/SocialMedia/SocialMediaNode.jsx
@@ -4,21 +4,21 @@ import Radium from 'radium';
 
 class SocialMediaNode extends React.Component {
   // Constructor used in ES6
-	constructor(props) {
+  constructor(props) {
     super(props);
   }
 
   render () {
-		return (
+    return (
       <li key='SocialMediaNode' id={this.props.name}>
-	      <a key='SocialMediaLink' className={this.props.className} style={styles.SocialMediaLink} href={this.props.link}>
-		      <span className='replaced-text' style={styles.ReplacedText} >
-						{this.props.name}
-					</span>
-				</a>
+      <a key='SocialMediaLink' className={this.props.className} style={styles.SocialMediaLink} href={this.props.link}>
+      <span className='replaced-text' style={styles.ReplacedText} >
+      {this.props.name}
+      </span>
+      </a>
       </li>
-		);
-	}
+    );
+  }
 }
 
 const styles = {};
