62b5d401d06cd93d3750138c30462911c720181b
Edwin Guzman <edwin.gzmn@gmail.com>
2016-06-27 16:40:27 -0400

    ESlinting more files.

--- src/app/components/Book/Book.jsx
+++ src/app/components/Book/Book.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+// import radium from 'radium';
 
 const Book = (props) => {
   const handleClick = (e) => e.preventDefault();
-
   const book = props.book;
   const bookImgSrc = book.item.imageSlug;
   const fullImgSrc = bookImgSrc !== 'No Image' ?
-    `https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?&userID=NYPL49807` +
+    'https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?&userID=NYPL49807' +
     `&password=CC68707&Value=${bookImgSrc}&content=M&Return=1&Type=M`
     : '/browse/recommendations/staff-picks/src/client/images/book-place-holder.png';
 
   return (
-    <div className={props.className} style={[props.style]}>
-      <a href='#' onClick={handleClick}>
-        <img style={props.style}
+    <div className={props.className}>
+      <a href="#" onClick={handleClick}>
+        <img
           alt={book.item.title}
           src={fullImgSrc}
           height={props.height}
-          width={props.width} />
+          width={props.width}
+        />
       </a>
     </div>
   );
@@ -26,7 +27,6 @@ const Book = (props) => {
 Book.propTypes = {
   book: React.PropTypes.object,
   className: React.PropTypes.string,
-  style: React.PropTypes.object,
   height: React.PropTypes.string,
   width: React.PropTypes.string,
 };
