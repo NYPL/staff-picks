42a3611de4bcbb164bdb91ec3d93da2ce71d11da
Edwin Guzman <edwin.gzmn@gmail.com>
2016-07-27 11:30:10 -0400

    Moving error page code to scss file.

--- src/app/components/Error404Page/Error404Page.jsx
+++ src/app/components/Error404Page/Error404Page.jsx
@@ -37,13 +37,13 @@ class Error404Page extends React.Component {
   render() {
     return (
       <div className="error-container" style={styles.ErrorContainer}>
-        <h1 style={styles.ErrorTitle}>We&#39;re sorry...</h1>
-        <p style={styles.ErrorContent}>
+        <h1 className="error-title">We&#39;re sorry...</h1>
+        <p>
           The page you requested is either unavailable or you need permission to view the content.
         </p>
-        <p style={styles.ErrorContent}>
+        <p>
           If you can&#39;t find the page you&#39;re looking for, please try our
-          <a href="http://www.nypl.org/ask-nypl" style={styles.ErrorContentLink}>
+          <a href="http://www.nypl.org/ask-nypl">
             ASK NYPL
           </a>
           service.
