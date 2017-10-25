044939d85546325ab8dde00ff9d17f3fa4106c2a
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-25 17:08:39 -0400

    Adding test transition class and managing active filters in an array.

--- src/app/components/BookFilters/Filter.jsx
+++ src/app/components/BookFilters/Filter.jsx
@@ -7,13 +7,18 @@ import {
   XIcon,
 } from 'dgx-svg-icons';
 
-const ANIMATION_TIMEOUT = 500;
+const ANIMATION_TIMEOUT = 400;
 
 class Filter extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { icon: <CheckSoloIcon /> };
+    const active = this.props.active;
+    this.state = {
+      icon: active ? <XIcon /> : <CheckSoloIcon />,
+      active,
+      activeClass: active ? 'active' : '',
+    };
     this.onClick = this.onClick.bind(this);
   }
 
@@ -21,9 +26,14 @@ class Filter extends React.Component {
     // Since the list of filters get re-rendered, when the a new list is generated and it's
     // longer than the previous list, then we need to focus on the selected/deselected filter
     // once all the filters are mounted.
+    const {
+      filter,
+      focusId,
+    } = this.props;
+
     setTimeout(() => {
-      if (this.props.filter.id === this.props.focusId) {
-        ReactDOM.findDOMNode(this.refs[this.props.filter.id]).focus();
+      if (filter.id === focusId) {
+        ReactDOM.findDOMNode(this.refs[filter.id]).focus();
       }
     }, ANIMATION_TIMEOUT);
   }
@@ -32,25 +42,33 @@ class Filter extends React.Component {
     const {
       filter,
       focusId,
+      active,
     } = nextProps;
 
     // We want to set all back to the Check icon.
-    if (!filter.active) {
-      this.setState({
-        icon: <CheckSoloIcon />,
-      });
-    }
+    const initIcon = active ? <XIcon /> : <CheckSoloIcon />;
+    this.setState({
+      icon: initIcon,
+      activeClass: active ? 'active' : '',
+    });
 
-    // If the focusId, the filter that was JUST selected/deselected, matches the filter,
+    // If the focusId, the id of the filter that was JUST selected/deselected, matches the filter,
     // then load the animation SVG and remove it shortly after. Also focus on it whether it was
-    // selected or deselected for accessibility.
+    // selected or deselected for accessibility. We then want to add the appropriate SVG based
+    // on whether it is active or not.
     if (filter.id === focusId) {
-      this.setState({ icon: <DotsIcon /> });
+      this.setState({
+        icon: <DotsIcon />,
+        activeClass: 'transition',
+      });
 
       setTimeout(() => {
-        const icon = filter.active ? <XIcon /> : <CheckSoloIcon />;
+        const icon = active ? <XIcon /> : <CheckSoloIcon />;
 
-        this.setState({ icon });
+        this.setState({
+          icon,
+          activeClass: active ? 'active' : '',
+        });
 
         ReactDOM.findDOMNode(this.refs[filter.id]).focus();
       }, ANIMATION_TIMEOUT);
@@ -58,14 +76,23 @@ class Filter extends React.Component {
   }
 
   onClick() {
-    const { filter } = this.props;
-    this.props.onClick(filter.id, !filter.active);
+    const {
+      filter,
+      active,
+    } = this.props;
+    this.props.onClick(filter.id, !active);
   }
 
   render() {
-    const { icon } = this.state;
-    const { filter } = this.props;
-    const activeClass = filter.active ? 'active' : '';
+    const {
+      icon,
+      activeClass,
+    } = this.state;
+    const {
+      filter,
+      // active,
+    } = this.props;
+    // const activeClass = active ? 'active' : '';
 
     return (
       <li className="filter-item">
@@ -86,6 +113,7 @@ Filter.propTypes = {
   filter: PropTypes.object,
   onClick: PropTypes.func,
   focusId: PropTypes.string,
+  active: PropTypes.bool,
 };
 
 export default Filter;
