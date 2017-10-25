6d3870198ec64a2285eef5aaad906cc87b42614b
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-25 12:16:21 -0400

    Addign correct url links in the About component.

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Sidebar = () => (
   <div className="about">
     <h3>Additional Information</h3>
-    <p>Many of these titles are available in formats for <a href="">patrons
-    with print disabilities.</a></p>
-    <p><a href="">About Best Books</a></p>
+    <p>Many of these titles are available in formats
+      for <a href="https://www.nypl.org/accessibility/print-disabilities">patrons
+      with print disabilities.</a></p>
+    <p><a href="https://www.nypl.org/books-music-dvds/recommendations/about/annual-lists">About
+    Best Books</a>
+    </p>
     <ul>
       <li>Facebook icon</li>
       <li>Twitter icon</li>
