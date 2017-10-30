0d04e2d7e9ca0cefc4309c6c848e8725c6a092f1
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-30 12:29:45 -0400

    Updating the JSON data and updating variable names.

--- src/app/components/Book/Book.jsx
+++ src/app/components/Book/Book.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import config from '../../../../appConfig.js';
-import utils from '../../utils/utils';
 
 const Book = ({ pick }) => {
-  const fullImgSrc = (!pick.imageSlug || pick.imageSlug === '') ? `${config.baseUrl}src/client/images/book-place-holder.png` :
-    'https://contentcafe2.btol.com/ContentCafe/Jacket.aspx' +
-    `?&userID=NYPL49807&password=CC68707&Value=${pick.imageSlug}&content=M&Return=1&Type=M`;
+  const fullImgSrc = (!pick.book.imageUrl || pick.book.imageUrl === '') ?
+    `${config.baseUrl}src/client/images/book-place-holder.png` : pick.book.imageUrl;
   const tagArray = pick.tags.map(tag => tag.toLowerCase().split(' ').join('-'));
   const tagClasses = tagArray.join(' ');
 
@@ -22,7 +20,7 @@ const Book = ({ pick }) => {
         <h4>
           <a href="#">{pick.book.title}</a>
         </h4>
-        <p>{pick.book.text}</p>
+        <p>{pick.reviews.text}</p>
       </div>
     </li>
   );
--- src/app/components/BookList/BookList.jsx
+++ src/app/components/BookList/BookList.jsx
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 import Book from '../Book/Book.jsx';
 
 const BookList = (props) => {
-  // const renderBookItems = (currentBooks) => (
-  //   currentBooks.length ? currentBooks.map((book, i) => <Book key={i} book={book} />) : null
-  // );
+  const renderBookItems = (currentBooks) => (
+    currentBooks.length ? currentBooks.map((book, i) => <Book key={i} pick={book} />) : null
+  );
 
-console.log(props.picks);
   return (
     <div className="booklist nypl-column-three-quarters">
       <h2>2017 Picks</h2>
       <ul className="nypl-row">
+        {renderBookItems(props.picks)}
       </ul>
     </div>
   );
