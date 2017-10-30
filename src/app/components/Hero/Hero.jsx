f6375b8a5d4afcae14e4ca150d2d496f4a8bb273
Kevin Friedman <kevinfriedman@nypl.org>
2017-10-30 16:16:29 -0400

    Refactor hero component to use data

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -2,85 +2,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import config from '../../../../appConfig.js';
-
-const { baseUrl } = config;
-const styles = {
-  childrens: {
-    backgroundImage: `url("${baseUrl}src/client/images/desktop.childrens100.FIN.png")`,
-  },
-  ya: {
-    backgroundImage: `url("${baseUrl}src/client/images/desktop.banner.YA.FIN.png")`,
-  },
-};
-
 class Hero extends React.Component {
   constructor(props) {
     super(props);
-
-    this.getHeroData = this.getHeroData.bind(this);
-    this.getType = this.getType.bind(this);
-  }
-  getHeroData(selection) {
-    let type = '';
-
-    if (selection) {
-      type = selection;
-    }
-
-    const heroData = {
-      childrens: {
-        type: 'childrens',
-        title: 'RECOMMENDATIONS',
-        description: 'Children\'s Books',
-        intro: 'Explore our annual selection of 100 notable titles for reading and sharing.',
-        image: `${baseUrl}src/client/images/c100.OG.png`,
-        url: `http://www.nypl.org${baseUrl}annual/childrens`,
-      },
-      ya: {
-        type: 'ya',
-        title: 'RECOMMENDATIONS',
-        description: 'Best Books for Teens',
-        intro: 'Explore our annual selection of outstanding young adult titles.',
-        image: `${baseUrl}src/client/images/YA.OG.png`,
-        url: `http://www.nypl.org${baseUrl}annual/ya`,
-      },
-    };
-
-    return heroData[type];
-  }
-
-  getType() {
-    const clientParam = (this.props.annualList && this.props.params) ? this.props.params.type : '';
-    const route = this.props.location.pathname || clientParam;
-
-    if (route.indexOf('childrens') !== -1) {
-      return 'childrens';
-    }
-
-    if (route.indexOf('ya') !== -1) {
-      return 'ya';
-    }
-
-    // Temporary default:
-    return 'childrens';
   }
 
   render() {
-    const heroData = this.getHeroData(this.getType());
-    const bannerStyle = styles[heroData.type];
-
     return (
-      <div className="Hero" style={bannerStyle}>
+      <div
+        className="Hero"
+        style={{ backgroundImage: `url("${this.props.currentPicks.heroUrl}")` }}
+      >
         <div className="Hero__container nypl-full-width-wrapper">
           <div className="Hero__text nypl-column-three-quarters nypl-column-offset-one">
             <div className="Hero__text__HeroTitle">
-              <h3>{heroData.title}</h3>
+              <h3>{this.props.currentPicks.type}</h3>
               <p className="Hero__text__HeroTitle__des">
-                {heroData.description}
+                {this.props.currentPicks.title}
               </p>
               <p className="Hero__text__HeroTitle__intro">
-                {heroData.intro}
+                {this.props.currentPicks.description}
               </p>
             </div>
           </div>
@@ -91,9 +32,7 @@ class Hero extends React.Component {
 }
 
 Hero.propTypes = {
-  params: PropTypes.object,
-  location: PropTypes.object,
-  annualList: PropTypes.bool,
+  currentPicks: PropTypes.object.isRequired,
 };
 
 export default Hero;
