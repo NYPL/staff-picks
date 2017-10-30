9c5a8415e5bbb5ebf60d52b07aa482c94d7cdb7e
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-30 12:11:12 -0400

    Testing adding new json file.

--- src/app/components/Book/Book.jsx
+++ src/app/components/Book/Book.jsx
@@ -3,33 +3,33 @@ import PropTypes from 'prop-types';
 import config from '../../../../appConfig.js';
 import utils from '../../utils/utils';
 
-const Book = ({ book }) => {
-  const fullImgSrc = (!book.imageSlug || book.imageSlug === '') ? `${config.baseUrl}src/client/images/book-place-holder.png` :
+const Book = ({ pick }) => {
+  const fullImgSrc = (!pick.imageSlug || pick.imageSlug === '') ? `${config.baseUrl}src/client/images/book-place-holder.png` :
     'https://contentcafe2.btol.com/ContentCafe/Jacket.aspx' +
-    `?&userID=NYPL49807&password=CC68707&Value=${book.imageSlug}&content=M&Return=1&Type=M`;
-  const tagArray = book.tags.map(tag => tag.toLowerCase().split(' ').join('-'));
+    `?&userID=NYPL49807&password=CC68707&Value=${pick.imageSlug}&content=M&Return=1&Type=M`;
+  const tagArray = pick.tags.map(tag => tag.toLowerCase().split(' ').join('-'));
   const tagClasses = tagArray.join(' ');
 
   return (
     <li
       className={`book-item nypl-column-half ${tagClasses}`}
-      key={book.title.trim()}
+      key={pick.book.title.trim()}
     >
       <div className="book-item-image-box">
         <img alt="" src={fullImgSrc} />
       </div>
       <div className="book-item-description">
         <h4>
-          <a href="#">{book.title}</a>
+          <a href="#">{pick.book.title}</a>
         </h4>
-        <p>{book.text}</p>
+        <p>{pick.book.text}</p>
       </div>
     </li>
   );
 };
 
 Book.propTypes = {
-  book: PropTypes.object,
+  pick: PropTypes.object,
 };
 
 export default Book;
