5a737ec076812bd23db8df7d04e1496dd2bef594
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-30 14:02:12 -0400

    Moving about urls to appConfig file.

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import appConfig from '../../../../appConfig';
+
+const { aboutUrls } = appConfig;
+
 const Sidebar = () => (
   <div className="about nypl-row">
     <div className="inner">
       <h3>Additional Information</h3>
-      <p>Many of these titles are available in formats
-        for <a href="https://www.nypl.org/accessibility/print-disabilities">patrons
-        with print disabilities.</a></p>
-      <p><a href="https://www.nypl.org/books-music-dvds/recommendations/about/annual-lists">About
-      Best Books</a>
-      </p>
+      <p>Many of these titles are available in formats for <a href={aboutUrls.print}>patrons
+      with print disabilities.</a></p>
+      <p><a href={aboutUrls.about}>About Best Books</a></p>
       <ul>
         <li>Facebook icon</li>
         <li>Twitter icon</li>
