a3cc89caa2255d38b551e8ac0c740f7a167720b9
Edwin Guzman <edwin.gzmn@gmail.com>
2017-10-31 11:37:59 -0400

    Making the h3 in the about component an h2.

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -7,7 +7,7 @@ const { aboutUrls } = appConfig;
 const Sidebar = () => (
   <div className="about nypl-row">
     <div className="inner">
-      <h3>Additional Information</h3>
+      <h2>Additional Information</h2>
       <p>Many of these titles are available in formats for <a href={aboutUrls.print}>patrons
       with print disabilities.</a></p>
       <p><a href={aboutUrls.about}>About Best Books</a></p>
