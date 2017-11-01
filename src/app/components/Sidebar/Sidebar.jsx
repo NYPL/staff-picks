f7a78bfdab82fcc45812f70351ac4271dc6dace0
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-11-01 15:34:24 -0400

    Added function to render filters only when js is enabled

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -1,48 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Transition } from 'react-transition-group'
 import { LeftWedgeIcon } from 'dgx-svg-icons';
 
 import BookFilters from '../BookFilters/BookFilters.jsx';
 import config from '../../../../appConfig';
 
-// const defaultStyle = {
-//   transition: `opacity ${duration}ms ease-in-out`,
-//   opacity: 0,
-// };
-//
-// const transitionStyles = {
-//   entering: { opacity: 0 },
-//   entered: { opacity: 1 },
-// };
-
-
 const Sidebar = (props) => {
-  const Fade = ({ inProp, duration = 300 }) => {
-    const defaultStyle = {
-      transition: `opacity ${duration}ms ease-in-out`,
-      opacity: 0,
-    };
-
-    const transitionStyles = {
-      entering: { opacity: 0 },
-      entered: { opacity: 1 },
-    };
+  const renderBookFilters = (shouldDisplay) => {
+    if (!shouldDisplay) {
+      return null;
+    }
 
     return (
-      <Transition in={inProp} timeout={duration}>
-        {(state) => (
-          <div style={Object.assign({}, defaultStyle, transitionStyles)}>
-            <BookFilters
-              filters={props.filters}
-              selectableFilters={props.selectableFilters}
-              setSelectedFilter={props.setSelectedFilter}
-              clearFilters={props.clearFilters}
-              selectedFilters={props.selectedFilters}
-            />
-          </div>
-        )}
-      </Transition>
+      <BookFilters
+        filters={props.filters}
+        selectableFilters={props.selectableFilters}
+        setSelectedFilter={props.setSelectedFilter}
+        clearFilters={props.clearFilters}
+        selectedFilters={props.selectedFilters}
+      />
     );
   };
 
@@ -51,7 +27,7 @@ const Sidebar = (props) => {
       <a href={config.recommendationsLink.url} className="back-link">
         <LeftWedgeIcon /> {config.recommendationsLink.label}
       </a>
-      <Fade in={props.isJsEnabled} />
+      {renderBookFilters(props.isJsEnabled)}
     </div>
   );
 };
