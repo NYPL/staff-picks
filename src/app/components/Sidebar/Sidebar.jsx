93c197ff1c24d8f9d0e5e38ae63cfa3db8b0e96e
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-11-01 14:16:53 -0400

    adding css-transition-group and boolean check to render filters

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group'
 import { LeftWedgeIcon } from 'dgx-svg-icons';
 
 import BookFilters from '../BookFilters/BookFilters.jsx';
 import config from '../../../../appConfig';
 
-const Sidebar = (props) => (
-  <div className="sidebar nypl-column-one-quarter">
-    <a href={config.recommendationsLink.url} className="back-link">
-      <LeftWedgeIcon /> {config.recommendationsLink.label}
-    </a>
+// const defaultStyle = {
+//   transition: `opacity ${duration}ms ease-in-out`,
+//   opacity: 0,
+// };
+//
+// const transitionStyles = {
+//   entering: { opacity: 0 },
+//   entered: { opacity: 1 },
+// };
 
-    <BookFilters
-      filters={props.filters}
-      selectableFilters={props.selectableFilters}
-      setSelectedFilter={props.setSelectedFilter}
-      clearFilters={props.clearFilters}
-      selectedFilters={props.selectedFilters}
-    />
-  </div>
-);
+
+const Sidebar = (props) => {
+  const Fade = ({ inProp, duration = 300 }) => {
+    const defaultStyle = {
+      transition: `opacity ${duration}ms ease-in-out`,
+      opacity: 0,
+    };
+
+    const transitionStyles = {
+      entering: { opacity: 0 },
+      entered: { opacity: 1 },
+    };
+
+    return (
+      <Transition in={inProp} timeout={duration}>
+        {(state) => (
+          <div style={Object.assign({}, defaultStyle, transitionStyles)}>
+            <BookFilters
+              filters={props.filters}
+              selectableFilters={props.selectableFilters}
+              setSelectedFilter={props.setSelectedFilter}
+              clearFilters={props.clearFilters}
+              selectedFilters={props.selectedFilters}
+            />
+          </div>
+        )}
+      </Transition>
+    );
+  };
+
+  return (
+    <div className="sidebar nypl-column-one-quarter">
+      <a href={config.recommendationsLink.url} className="back-link">
+        <LeftWedgeIcon /> {config.recommendationsLink.label}
+      </a>
+      <Fade in={props.isJsEnabled} />
+    </div>
+  );
+};
 
 Sidebar.propTypes = {
   filters: PropTypes.array,
