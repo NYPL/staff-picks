269f98ebc85940df4aadb3c366490a476d80aba6
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-11-01 15:43:12 -0400

    Added tests

--- test/unit/Sidebar.test.js
+++ test/unit/Sidebar.test.js
@@ -6,12 +6,12 @@ import { mount } from 'enzyme';
 import Sidebar from '../../src/app/components/Sidebar/Sidebar.jsx';
 
 describe('Sidebar', () => {
-  describe('Default component', () => {
+  describe('Default component with JS turned on', () => {
     let component;
 
     before(() => {
       component = mount(
-        <Sidebar />
+        <Sidebar isJsEnabled={true} />
       );
     });
 
@@ -32,6 +32,26 @@ describe('Sidebar', () => {
 
     it('should render a <BookFilters /> component', () => {
       expect(component.find('BookFilters')).to.have.length(1);
+    });
+  });
+
+  describe('Default component with JS turned off', () => {
+    let component;
+
+    before(() => {
+      component = mount(
+        <Sidebar isJsEnabled={false} />
+      );
+    });
+
+    it('should render .sidebar wrapper DOM', () => {
+      const sidebarWrapper = component.find('div').at(0);
+      expect(sidebarWrapper.hasClass('sidebar')).to.equal(true);
+      expect(sidebarWrapper.hasClass('nypl-column-one-quarter')).to.equal(true);
+    });
+
+    it('should not render the <BookFilters /> component', () => {
+      expect(component.find('BookFilters')).to.have.length(0);
     });
   });
 });
