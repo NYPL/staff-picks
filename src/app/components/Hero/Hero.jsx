e31d9e93e50e6220aa8b3653655774b1988ad10b
ricardo galvez <ricardogalvez@nypl.org>
2017-11-03 15:50:24 -0400

    :meat_on_bone: works toward adding in new images for background: * new artwork * style changes to accomodate new artworks

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -10,17 +10,17 @@ class Hero extends React.Component {
   render() {
     return (
       <div
-        className="Hero"
+        className="hero"
         style={{ backgroundImage: `url("${this.props.heroData.heroImageUrl}")` }}
       >
-        <div className="Hero__container nypl-full-width-wrapper">
-          <div className="Hero__text nypl-column-three-quarters nypl-column-offset-one">
-            <div className="Hero__text__HeroTitle">
+        <div className="hero__container nypl-full-width-wrapper">
+          <div className="hero__text nypl-column-three-quarters nypl-column-offset-one">
+            <div className="hero__text__HeroTitle">
               {this.props.heroData.category ? <p>{this.props.heroData.category}</p> : ''}
-              <h1 className="Hero__text__HeroTitle__des">
+              <h1 className="hero__text__HeroTitle__des">
                 {this.props.heroData.header}
               </h1>
-              <p className="Hero__text__HeroTitle__intro">
+              <p className="hero__text__heroTitle__intro">
                 {this.props.heroData.description}
               </p>
             </div>
