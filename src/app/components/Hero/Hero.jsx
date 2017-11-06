e5f4631062dcd81fbf3ec4f065da3f1c43154f1a
Edwin Guzman <edwin.gzmn@gmail.com>
2017-11-06 13:35:53 -0500

    Fixing component structure for the Hero.

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -2,35 +2,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Hero extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const image = this.props.heroData.heroImageUrl;
-    return (
-      <div className="hero">
-        <div className="hero-image">
-          <img src={image} alt="" />
-        </div>
-        <div className="hero-container nypl-full-width-wrapper">
-          <div className="nypl-row">
-            <div className="hero-content nypl-column-three-quarters nypl-column-offset-one">
-              <h1>{this.props.heroData.header}</h1>
-              <p className="hero-content-description">
-                {this.props.heroData.description}
-              </p>
-            </div>
+const Hero = ({ heroData }) => {
+  const image = heroData.heroImageUrl;
+  return (
+    <div className="hero">
+      <div className="hero-image">
+        <img src={image} alt="" />
+      </div>
+      <div className="hero-container nypl-full-width-wrapper">
+        <div className="nypl-row">
+          <div className="hero-content nypl-column-three-quarters nypl-column-offset-one">
+            <h1>{heroData.header}</h1>
+            <p className="hero-content-description">
+              {heroData.description}
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Hero.propTypes = {
-  currentPicks: PropTypes.object.isRequired,
   heroData: PropTypes.object.isRequired,
 };
 
