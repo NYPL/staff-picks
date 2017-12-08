f8b8fe7e941621ef655e42819b0166ad2877dd97
Kevin Friedman <kevinfriedman@nypl.org>
2017-12-08 15:02:59 -0500

    Add publisher and isbns

--- src/app/components/RelatedBibs/RelatedBibs.jsx
+++ src/app/components/RelatedBibs/RelatedBibs.jsx
@@ -4,6 +4,34 @@ import axios from 'axios';
 import testData from './testData.js';
 import BookStore from '../../stores/BookStore';
 
+function extractIsbns(bib) {
+  var isbns = [];
+
+  bib.varFields.forEach((varField) => {
+    if (varField.fieldTag === 'i' && varField.marcTag === '020') {
+      const isbn = varField.subfields[0].content.substr(0, varField.subfields[0].content.indexOf(' '));
+
+      if (isbn) {
+        isbns.push(isbn);
+      }
+    }
+  });
+
+  return isbns;
+}
+
+function extractPublisher(bib) {
+  var publisher = '';
+
+  bib.varFields.forEach((varField) => {
+    if (varField.fieldTag === 'b' && varField.marcTag === '710') {
+      publisher = varField.subfields[0].content;
+    }
+  });
+
+  return publisher;
+}
+
 class RelatedBibs extends React.Component {
   constructor(props) {
     super(props);
@@ -27,17 +55,20 @@ class RelatedBibs extends React.Component {
   onChange() {}
 
   renderBib(bib, i) {
+    let isbns = extractIsbns(bib);
+    let publisher = extractPublisher(bib)
+
     return (
       <div key={i}>
+        <img src={`https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?&userID=NYPL49807&password=CC68707&Value=${isbns[0]}&content=S&Return=1&Type=M`} alt="" />
         <h3>{bib.title}</h3>
         <p>{bib.author}</p>
+        <p>{publisher}</p>
       </div>
     );
   }
 
   render() {
-    console.log(this.state.bibs)
-
     if (!this.state.bibs && !this.state.bibs.length) {
       return <div>Loading...</div>;
     }
