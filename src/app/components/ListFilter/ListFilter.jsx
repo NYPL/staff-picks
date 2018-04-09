ddab6a7bc6ddead9bf979b1273e3fa5337313ee2
Kang Peng <kangtingpeng@nypl.org>
2018-04-09 17:19:50 -0400

    Added tests.

--- src/app/components/ListFilter/ListFilter.jsx
+++ src/app/components/ListFilter/ListFilter.jsx
@@ -36,10 +36,11 @@ class ListFilter extends React.Component {
     const visuallyHidden = (this.state.withJS) ? 'visuallyHidden' : '';
     const selectName = fieldsetProps.fieldsetName;
     const selectId = `${selectName}-input`;
-    const defaultValue = fieldsetProps.options[0].value;
-    const optionList = fieldsetProps.options.map(
+    const defaultValue = (fieldsetProps.options.length && fieldsetProps.options[0].value) ?
+      fieldsetProps.options[0].value : '';
+    const optionList = (fieldsetProps.options.length) ? fieldsetProps.options.map(
       (opt) => <option value={opt.value} key={opt.value}>{opt.name}</option>
-    );
+    ) : null;
 
     return (
       <fieldset>
---
+++ test/unit/ListFilter.test.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import ListFilter from '../../src/app/components/ListFilter/ListFilter.jsx';
+
+const fieldsetProps = {
+  fieldsetName: 'season',
+  options: [
+    { name: '2018 Winter', value: '2018-01-01' },
+    { name: '2017 Fall', value: '2017-09-01' },
+    { name: '2017 Summer', value: '2017-06-01' },
+    { name: '2017 Spring', value: '2017-04-01' },
+  ],
+};
+
+describe('ListFilter', () => {
+  describe('Default', () => {
+    let component;
+
+    before(() => {
+      component = shallow(<ListFilter fieldsetProps={fieldsetProps} />);
+    });
+
+    it('should render a fieldset, and inside the fieldset, it should render <label> and <select>.',
+      () => {
+        expect(component.find('fieldset').length).to.equal(1);
+        expect(component.find('label').length).to.equal(1);
+        expect(component.find('select').length).to.equal(1);
+      }
+    );
+
+    it('should render the <option>. The number of the option equals to the length of the data has.',
+      () => {
+        expect(component.find('option').length).to.equal(4);
+      }
+    );
+  });
+});
