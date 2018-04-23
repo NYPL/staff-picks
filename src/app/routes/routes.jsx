772a08342ac4b4523d15d7992a0b4acb7cfb6783
Ho-Ling Poon <holingpoon@nypl.org>
2018-04-23 12:32:52 -0400

    WWW-17 Adding routes to separate Best Books and Staff Picks.

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -7,7 +7,7 @@ import { IndexRoute, Route, Redirect } from 'react-router';
 import App from '../components/Application/Application.jsx';
 import Main from '../components/Application/Main.jsx';
 import BookPage from '../components/BookPage/BookPage.jsx';
-// import StaffPicks from '../components/StaffPicks/StaffPicks.jsx';
+import StaffPicks from '../components/StaffPicks/StaffPicks.jsx';
 import Error404Page from '../components/Error404Page/Error404Page.jsx';
 
 import appConfig from '../../../appConfig';
@@ -17,13 +17,14 @@ const routes = {
   client: (
     <Route path={`${appConfig.baseUrl}`} component={App}>
       <IndexRoute component={Main} />
-      {/*<Route path="/books-music-movies/recommendations/best-books/staff-picks/" component={StaffPicks} />*/}
-      {/*<Route path={`${appConfig.baseMonthUrl}:month`} component={StaffPicks} />*/}
-      {/*<Route path={`${appConfig.baseMonthUrl}:month/:id`} component={BookPage} />*/}
-      <Route path={`${appConfig.baseUrl}404`} component={Error404Page} />
-      <Route path={`${appConfig.baseUrl}:type`} component={Main} />
-      <Route path={`${appConfig.baseUrl}:type/:year`} component={Main} />
-      <Route path={`${appConfig.baseUrl}:type/:year/:id`} component={BookPage} />
+      <Route path={`${appConfig.baseRouteMonthUrl}`} component={StaffPicks} />
+      <Route path={`${appConfig.baseRouteMonthUrl}404`} component={Error404Page} />
+      <Route path={`${appConfig.baseRouteMonthUrl}:month`} component={StaffPicks} />
+      <Route path={`${appConfig.baseRouteMonthUrl}:month/:id`} component={BookPage} />
+      <Route path={`${appConfig.baseAnnualUrl}404`} component={Error404Page} />
+      <Route path={`${appConfig.baseAnnualUrl}:type`} component={Main} />
+      <Route path={`${appConfig.baseAnnualUrl}:type/:year`} component={Main} />
+      <Route path={`${appConfig.baseAnnualUrl}:type/:year/:id`} component={BookPage} />
       <Redirect from="*" to={`${appConfig.baseUrl}`} />
     </Route>
   ),
