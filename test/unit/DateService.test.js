8a878ad07bf672bfe8885d1446ee4b3b820d0c52
Kang Peng <kangtingpeng@nypl.org>
2018-04-23 14:38:25 -0400

    Added the test to utils/DateService when no valid date passed.

--- test/unit/DateService.test.js
+++ test/unit/DateService.test.js
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme';
 
 import DateService from '../../src/app/utils/DateService.js';
 
+describe('When no date is passed', () => {
+  it('should return the object that has empty month and year values.', () => {
+    expect(DateService(undefined)).to.deep.equal({ month: '', year: '' });
+  });
+});
+
 describe('When the passed year is earlier than 2016', () => {
   it('should return the correct month and year.', () => {
     expect(DateService('2015-04-01')).to.deep.equal({ month: 'April', year: 2015 });
