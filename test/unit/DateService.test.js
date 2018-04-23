7c8a55ee4aa25985cd22678233f123e3d9cc03c7
Kang Peng <kangtingpeng@nypl.org>
2018-04-23 14:03:56 -0400

    Added the test for utils/DateService

---
+++ test/unit/DateService.test.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import DateService from '../../src/app/utils/DateService.js';
+
+describe('When the passed year is earlier than 2016', () => {
+  it('should return the correct month and year.', () => {
+    expect(DateService('2015-04-01')).to.deep.equal({ month: 'April', year: 2015 });
+  });
+});
+
+describe('When the passed year is later than 2016', () => {
+  it('should return month value as the season with the correct year.', () => {
+    expect(DateService('2016-06-01')).to.deep.equal({ month: 'Summer', year: 2016 });
+  });
+});
