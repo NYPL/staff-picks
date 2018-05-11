7429148e24d85787e1e6f1f0045b919eca6d41d1
Kang Peng <kangtingpeng@nypl.org>
2018-05-11 16:46:27 -0400

    Imported visuallyHidden instead of making a copy.

--- test/unit/Hero.test.js
+++ test/unit/Hero.test.js
@@ -35,18 +35,18 @@ describe('Hero', () => {
       component.unmount();
     });
 
-    it('should has the correct title and description.', () => {
+    it('should have the correct title and description.', () => {
       expect(component.find('h1').text()).to.equal('Best Books for Teens');
       expect(component.find('p.hero-content-description').text()).to.equal(
         'Explore our annual selection of outstanding young adult titles.'
       );
     });
 
-    it('should not has the section title.', () => {
+    it('should not have the section title.', () => {
       expect(component.find('p.hero-section-title').length).to.equal(0);
     });
 
-    it('should has the correct image link.', () => {
+    it('should have the correct image link.', () => {
       expect(component.find('img').node.props.src).to.equal(
         'src/client/images/desktop.teens.cover.2017.png'
       );
@@ -65,18 +65,18 @@ describe('Hero', () => {
       component.unmount();
     });
 
-    it('should has the correct title and description.', () => {
+    it('should have the correct title and description.', () => {
       expect(component.find('h1').text()).to.equal('Best Books for Kids');
       expect(component.find('p.hero-content-description').text()).to.equal(
         'Explore our annual selection of outstanding children\'s titles.'
       );
     });
 
-    it('should not has the section title.', () => {
+    it('should not have the section title.', () => {
       expect(component.find('p.hero-section-title').length).to.equal(0);
     });
 
-    it('should has the correct image link.', () => {
+    it('should have the correct image link.', () => {
       expect(component.find('img').node.props.src).to.equal(
         'src/client/images/desktop.kids.cover.2017.png'
       );
@@ -95,19 +95,19 @@ describe('Hero', () => {
       component.unmount();
     });
 
-    it('should has the correct title and description.', () => {
+    it('should have the correct title and description.', () => {
       expect(component.find('h1').text()).to.equal('Staff Picks');
       expect(component.find('p.hero-content-description').text()).to.equal(
         'Nobody loves books more than our experts. Browse and filter hundreds of their favorites.'
       );
     });
 
-    it('should has the correct section title.', () => {
+    it('should have the correct section title.', () => {
       expect(component.find('p.hero-section-title').length).to.equal(1);
       expect(component.find('p.hero-section-title').text()).to.equal('BOOKS/MUSIC/MOVIES');
     });
 
-    it('should has the correct style for backgeround and texts.', () => {
+    it('should have the correct style for backgeround and texts.', () => {
       expect(component.find('div.hero').node.props.className).to.equal('hero staff-picks-hero');
     });
 
