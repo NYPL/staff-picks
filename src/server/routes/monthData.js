1c169c6d2be04f4d2232474874307d494e023233
Gregory Kallenberg <gregorykallenberg@nypl.org>
2018-05-16 10:11:19 -0400

    add a check on year-month-day in path and redirect to year-month

--- src/server/routes/monthData.js
+++ src/server/routes/monthData.js
@@ -68,6 +68,18 @@ function currentMonthData(req, res, next) {
     });
 }
 
+/**
+ * Returns first two matches hyphenated for redirecting a three part date
+ * to a two part date in a URL.
+ * @param {object} match
+ * @param {integer} d1
+ * @param {integer} d2
+ * @return {string}
+ */
+function toMonthAndYear(match, d1, d2) {
+  return [d1, d2].join('-');
+}
+
 /* selectMonthData
  * Get a specific month's or season's staff pick list.
 * It calls '/book-lists?type=staff-picks' to get all the available list options first.
@@ -75,9 +87,16 @@ function currentMonthData(req, res, next) {
 function selectMonthData(req, res, next) {
   const listOptions = config.staffPicksListOptions;
   let seasonListOptions = [];
+  const dateRequest = req.params.time;
+
+  // Redirects older three part dates in URLs to the new two part date before validation.
+  if (dateRequest && /^(\d{4})-(\d{2})-(\d{2})$/.test(dateRequest)) {
+    const newPath = req.url.replace(/(\d{4})-(\d{2})-(\d{2})/, toMonthAndYear);
+    return res.redirect(newPath);
+  }
 
   // Checks if the URL input fits season's convention
-  const seasonMatches = matchListDate(req.params.time);
+  const seasonMatches = matchListDate(dateRequest);
   // Default audience list is the adult list
   let audience = 'Adult';
   let isValidAudience = true;
@@ -161,7 +180,15 @@ function selectMonthData(req, res, next) {
  * Gets a specific month's or season's staff pick list on the client side.
  */
 function selectClientMonthData(req, res) {
-  const seasonMatches = matchListDate(req.params.time);
+  const dateRequest = req.params.time;
+  // Redirects older three part dates in URLs to the new two part date before validation.
+  if (dateRequest && /^(\d{4})-(\d{2})-(\d{2})$/.test(dateRequest)) {
+    const newPath = req.url.replace(/(\d{4})-(\d{2})-(\d{2})/, toMonthAndYear);
+    return res.redirect(`${config.baseUrl}${newPath}`);
+  }
+
+  // Checks if the URL input fits season's convention
+  const seasonMatches = matchListDate(dateRequest);
   if (!seasonMatches) {
     logger.error('Status Code: 400, Error Message: Invalid season.');
 
